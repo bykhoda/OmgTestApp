a2e5a6210f970df49e61015f7034a97b8efb43a1
Dan <bykhoda@outlook.com>
2024-03-04 04:07:31 +0300

    No storyboards

--- OmgTestApp/SceneDelegate.swift
+++ OmgTestApp/SceneDelegate.swift
@@ -10,9 +10,14 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.frame = UIScreen.main.bounds
+        window.rootViewController = MainViewController()
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
